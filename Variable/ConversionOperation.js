dec4dac9f9befdff90bbe5c1e1855dbd6fc996e4
Ravi Kumar Kushwaha <163456484+ravi-kumar-kushwaha@users.noreply.github.com>
2024-10-05 14:05:06 +0000

    Oeration

---
+++ Variable/ConversionOperation.js
@@ -0,0 +1,21 @@
+let score = 50;
+console.log(typeof(score)) //number
+
+let string = String(score);   //number
+console.log(typeof(string));  //string
+
+
+//--------------operation--------------------
+console.log(5+4);//9
+console.log(5-4);//1
+console.log(5*4);//20
+console.log(5/4);//1.25
+console.log(5**4);//625
+console.log(5%4);//1
+
+let str1 = "Hello "
+let str2 = "Ravi Kumar Kushwaha"
+let str3 = str1+str2
+console.log(str3);  //Hello Ravi Kumar Kushwaha
+console.log(+true); //1
+console.log(+" "); //0
