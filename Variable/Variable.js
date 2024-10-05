34254e1b6525b7161f169287e2a8a4b27e473442
Ravi Kumar Kushwaha <163456484+ravi-kumar-kushwaha@users.noreply.github.com>
2024-10-05 13:37:40 +0000

    changes Variable

--- Variable/Variable.js
+++ Variable/Variable.js
@@ -4,3 +4,13 @@ const email = "ravi@gmail.com";
 const city = "Ballia";
 const state = "Uttar Pradesh";
 console.table([name,age,email,city,state]);
+/*
+ ┌─────────┬───────────────────────┐
+ │ (index) │ Values                │
+ ├─────────┼───────────────────────┤
+ │ 0       │ 'Ravi Kumar Kushwaha' │
+ │ 1       │ 25                    │
+ │ 2       │ 'ravi@gmail.com'      │
+ │ 3       │ 'Ballia'              │
+ │ 4       │ 'Uttar Pradesh'       │
+ */
