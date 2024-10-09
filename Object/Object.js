c6b22f4b615a61aa1df5c618c977fcf248a48eb7
Ravi Kumar Kushwaha <163456484+ravi-kumar-kushwaha@users.noreply.github.com>
2024-10-09 01:51:39 +0000

    Object1

--- Object/Object.js
+++ Object/Object.js
@@ -9,9 +9,9 @@ const user = {
     islogin:false,
     lastloginDays:["Monday","friday"]
 }
-console.log(user.name);//Ravi Kumar Kushwaha
-console.log(user['name']);//Ravi Kumar Kushwaha
-console.log(user);
+// console.log(user.name);//Ravi Kumar Kushwaha
+// console.log(user['name']);//Ravi Kumar Kushwaha
+// console.log(user);
 /**
      {
   name: 'Ravi Kumar Kushwaha',
@@ -40,9 +40,9 @@ console.log(user);
 Prevents the modification of existing property attributes and values, and prevents the addition of new properties.
 @param o — Object on which to lock the attributes.
  */
-Object.freeze(user);
-console.log(user.name="Virat Verma");//Virat Verma
-console.log(user);
+// Object.freeze(user);
+// console.log(user.name="Virat Verma");//Virat Verma
+// console.log(user);
 /**
    {
   name: 'Ravi Kumar Kushwaha',
@@ -53,3 +53,33 @@ console.log(user);
   lastloginDays: [ 'Monday', 'friday' ]
 }
  */
+ user.greet = function(){
+  console.log('Hello Java Script Users!')
+}
+console.log(user.greet());//Hello Java Script Users!
+console.log(user);
+// {
+//   name: 'Ravi Kumar Kushwaha',
+//   age: 25,
+//   location: 'Ballia',
+//   email: 'virat@gmail.com',
+//   islogin: false,
+//   lastloginDays: [ 'Monday', 'friday' ],
+//   greet: [Function (anonymous)]
+// }
+
+user.greetTwo = function(){
+  console.log(`Hello Java Script Users! ${user.name}`)
+}
+console.log(user.greetTwo());//Hello Java Script Users! Ravi Kumar Kushwaha
+console.log(user);
+// {
+//   name: 'Ravi Kumar Kushwaha',
+//   age: 25,
+//   location: 'Ballia',
+//   email: 'virat@gmail.com',
+//   islogin: false,
+//   lastloginDays: [ 'Monday', 'friday' ],
+//   greet: [Function (anonymous)],
+//   greetTwo: [Function (anonymous)]
+// }
