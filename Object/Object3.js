dd06bd51fd31e3aa73df93e40b8e0c1e9807e19f
Ravi Kumar Kushwaha <163456484+ravi-kumar-kushwaha@users.noreply.github.com>
2024-10-09 04:29:11 +0000

    Object3

---
+++ Object/Object3.js
@@ -0,0 +1,25 @@
+//Destructuring in Object
+
+const cource = {
+    subject:"Java Script",
+    price:9999,
+    teacher:"Ravi Kushwaha"
+}
+// const{subject} = cource;
+//console.log(subject);//Java Script
+// const{subject,price,teacher} = cource;
+// console.log(subject);//Java Script
+// console.log(price);//9999
+// console.log(teacher);//Ravi Kushwaha
+
+// const{subject:sub} = cource;
+// console.log(sub);//Java Script
+
+// const navbar = ({name})=>{
+
+// }
+// navbar(name="Ravi Kumar Kushwaha");
+
+//+++++++++++++++++++++++++++++++++++API+++++++++++++++++++++++++++++++++++++++++
+//random user api.  
+URL='https://randomuser.me/api/'
