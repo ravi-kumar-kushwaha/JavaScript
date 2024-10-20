59042d015adcf6249c6b463d6de09f697eef0a15
Ravi Kumar Kushwaha <163456484+ravi-kumar-kushwaha@users.noreply.github.com>
2024-10-20 12:49:43 +0000

    forInLoop

---
+++ Loops/forInloop.js
@@ -0,0 +1,21 @@
+const languages = {
+    'js':'javaScript',
+    'cpp':'c++',
+    "java":'java',
+    'py':'python',
+    'rb':"rubby",
+    'swwift':'swift by apple'
+}
+
+for (lan in languages){
+console.log(lan);
+
+}
+/**
+ js
+cpp
+java
+py
+rb
+swwift
+ */
